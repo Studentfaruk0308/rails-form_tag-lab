70a7b298b32029d95864d58e3ad0c0089d6ae977
faruk <studentfaruk0308@gmail.com>
2022-08-31 07:39:20 +1000

    31082022 0740

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Student.create(first_name: "Andre", last_name: "Hermanto")
